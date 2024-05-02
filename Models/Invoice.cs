9d3fc2e89f0abc1c86de02658157ae8ccd2e634f
Rawden Hoff <41743413+rawdenhoff@users.noreply.github.com>
2024-05-02 16:44:42 +0100

    Added Reporting

--- Models/Invoice.cs
+++ Models/Invoice.cs
@@ -6,4 +6,29 @@ public record Invoice
     public DateTime InvoiceDate { get; set; }
     public string CustomerName { get; set; }
     public List<InvoiceLine> Lines { get; set; }
+    public Invoice()
+    {
+        Lines = new List<InvoiceLine>();
+        Lines.Add(new InvoiceLine()
+        {
+            Qty = 1,
+            Description = "Description Placeholder",
+            Discount = 1,
+            Sku = "000001",
+            UnitPrice = 1.5M
+        });
+
+        Lines.Add(new InvoiceLine()
+        {
+            Qty = 4,
+            Description = "Description Placeholder 2",
+            Discount = 2,
+            Sku = "000002",
+            UnitPrice = 4.8M
+        });
+
+        this.Lines = Lines;
+        this.InvoiceDate = DateTime.Now;
+        this.CustomerName = "Mr R Williams";
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,17 +1,62 @@
 
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System.Diagnostics;
+using Telerik.Reporting;
+using Telerik.Reporting.Cache.File;
+using Telerik.Reporting.Services;
+using Telerik.WebReportDesigner.Services;
 using telerikReportingDemo.Components;
 using telerikReportingDemo.Implementations;
 using telerikReportingDemo.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages()
+     .AddNewtonsoftJson();
+builder.Services.AddServerSideBlazor();
+
+builder.Services.TryAddSingleton<IReportServiceConfiguration>(sp => new ReportServiceConfiguration
+{
+    ReportingEngineConfiguration = sp.GetService<IConfiguration>(),
+    HostAppId = "BlazorWebReportDesignerDemo",
+    Storage = new FileStorage(),
+    ReportSourceResolver = new UriReportSourceResolver(Path.Combine(sp.GetService<IWebHostEnvironment>().WebRootPath, "Reports"))
+});
+builder.Services.TryAddSingleton<IReportDesignerServiceConfiguration>(sp => new ReportDesignerServiceConfiguration
+{
+    DefinitionStorage = new FileDefinitionStorage(Path.Combine(sp.GetService<IWebHostEnvironment>().WebRootPath, "Reports")),
+    SettingsStorage = new FileSettingsStorage(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Telerik Reporting")),
+    ResourceStorage = new ResourceStorage(Path.Combine(sp.GetService<IWebHostEnvironment>().WebRootPath, "Resources")),
+    SharedDataSourceStorage = new FileSharedDataSourceStorage(Path.Combine(sp.GetService<IWebHostEnvironment>().WebRootPath, "Reports", "Shared Data Sources")),
+});
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddControllers().AddNewtonsoftJson();
+
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<ITenantIdentifier, TenantIdentifier>();
 builder.Services.AddScoped<IInvoiceService, InvoiceService>();
+
+builder.Services.AddTelerikBlazor();
+
+builder.Services.AddSingleton<IReportServiceConfiguration>(sp =>
+{
+    //var tenantIdentifierServiceFactory = sp.GetRequiredService<ITenantIdentifier>();
+    var serviceScopeFactory = sp.GetRequiredService<IServiceScopeFactory>();
+    var reportsPath = System.IO.Path.Combine(sp.GetService<IWebHostEnvironment>().ContentRootPath, "wwwroot", "Reports");
+    //reportsPath = "C:\\Program Files (x86)\\Progress\\Telerik Reporting 2024 Q1\\Report Designer\\Examples";
+    return new ReportServiceConfiguration
+    {
+        ReportingEngineConfiguration = ConfigurationHelper.ResolveConfiguration(sp.GetService<IWebHostEnvironment>()),
+        HostAppId = "ReportingCore3App",
+        Storage = new FileStorage(),
+        ReportSourceResolver = new ReportSourceResolver(reportsPath, serviceScopeFactory),
+        //ReportSourceResolver = new UriReportSourceResolver(reportsPath),
+        ReportSharingTimeout = 1400
+    };
+});
 
 var app = builder.Build();
 
@@ -24,11 +69,90 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
+app.UseRouting();
+
+
+
+
 app.UseAntiforgery();
+
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    
+});
+
+
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
+
+
 app.Run();
+
+
+static class ConfigurationHelper
+{
+    public static IConfiguration ResolveConfiguration(IWebHostEnvironment environment)
+    {
+        var reportingConfigFileName = System.IO.Path.Combine(environment.ContentRootPath, "appsettings.json");
+        return new ConfigurationBuilder()
+            .AddJsonFile(reportingConfigFileName, true)
+            .Build();
+    }
+}
+
+public class ReportSourceResolver : IReportSourceResolver
+{
+    Report gridReportInstance;
+    string reportPath;
+    private readonly IServiceScopeFactory serviceScopeFactory;
+
+    public ReportSourceResolver(
+        string reportsPath,
+        IServiceScopeFactory serviceScopeFactory
+        )
+    {
+        this.reportPath = reportsPath;
+        this.serviceScopeFactory = serviceScopeFactory;
+    }
+
+    public ReportSource Resolve(string report, OperationOrigin operationOrigin, IDictionary<string, object> currentParameterValues)
+    {
+        try
+        {
+
+            FileInfo fiReport = new FileInfo(System.IO.Path.Combine(this.reportPath, report));
+            var reportPackager = new ReportPackager();
+            using (var sourceStream = File.OpenRead(fiReport.FullName))
+            {
+                var reportDef = (Report)reportPackager.UnpackageDocument(sourceStream);
+                this.gridReportInstance = reportDef;
+            }
+
+            var toReturn = new InstanceReportSource()
+            {
+                ReportDocument = this.gridReportInstance
+            };
+
+            var dataSource = this.gridReportInstance.DataSource as ObjectDataSource;
+            var scope = serviceScopeFactory.CreateScope();
+            var invoiceService = scope.ServiceProvider.GetRequiredService<IInvoiceService>();
+
+            //                         not ideal code if exceptions are generated
+            var invoice = invoiceService.GetInvoiceAsync(1).Result;
+            dataSource.DataSource = invoice;
+
+            return toReturn;
+        }
+        catch (Newtonsoft.Json.JsonException jex)
+        {
+            Trace.TraceError("A problem occurred while deserializing the report source identifier: {0}.{1}Falling back to the next report resolver.", jex, Environment.NewLine);
+            return null;
+        }
+    }
+
+}
